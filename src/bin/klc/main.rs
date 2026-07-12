7cf54bb38ecbe71e2c6b323fad793bd876307ca5
KwachOjunga <ojungakwach@gmail.com>
2026-07-12 17:22:27 +0300

    Update lock file to use dependencies specified in crates instead of my own copies add module files for specifying the dialect in the compiler and for ast_lowering (#1)

--- src/bin/klc/main.rs
+++ src/bin/klc/main.rs
@@ -1,5 +1,7 @@
 // This will be the primary entry point of our compiler.
 // Just open files, read them into memory and find a way of ateast parsing them.
 mod ast;
+mod ast_lowering;
+mod dialect;
 
 fn main() {}
